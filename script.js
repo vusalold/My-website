aa28a56dc3ebd201a475f79212427347e2b9184e
vusal <vusal.mzade@gmail.com>
2026-09-04 19:22:29 +0400

    İlk veb-sayt

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function sayHello() {
+﻿function sayHello() {
     const message = document.getElementById('message');
     message.textContent = 'Salam! Vercel ilə tanış olmaq çox gözəldir! 🚀';
     message.style.color = '#ffd700';
