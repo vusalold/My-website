e91598716332576231efac53868a44fdb4c9419c
Vusal <vusal.mzade30@gmail.com>
2026-09-04 19:16:52 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function sayHello() {
+    const message = document.getElementById('message');
+    message.textContent = 'Salam! Vercel ilə tanış olmaq çox gözəldir! 🚀';
+    message.style.color = '#ffd700';
+}
